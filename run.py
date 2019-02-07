abc65f02b72becac2b64612d57ec46b7999dd313
mcolligan <matt.colligan@ed.ac.uk>
2019-02-07 12:03:10 +0000

    added run as main script

---
+++ run.py
@@ -0,0 +1,14 @@
+#
+# Main control script for mouse reaching task 
+#
+
+
+# Libraries
+import RPi.GPIO as GPIO     # raspberry pi pins
+
+
+# Pin setup
+paw_l = 1       # left paw touch sensor
+paw_r = 2       # right paw touch sensor
+
+
