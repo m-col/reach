23d82ca0132684b736b0e22735d280191cb741a0
mcolligan <matt.colligan@ed.ac.uk>
2019-02-09 20:05:55 +0000

    integrated states of task into main function along with params from config parser

--- run.py
+++ run.py
@@ -9,7 +9,13 @@ import RPi.GPIO as GPIO         # raspberry pi pins
 from datetime import datetime
 from time import sleep, time
 import signal
-from helpers import Spout, handle_signal
+import random
+import sys
+
+# Custom
+import spout
+import helpers
+from config import process_config
 
 
 ## Setup
@@ -25,25 +31,57 @@ paw_r = 27       # right paw touch sensor
 GPIO.setup(paw_l, GPIO.IN)
 GPIO.setup(paw_r, GPIO.IN)
 
-# Spouts
-if spout_count == 1:
-    spout1 = Spout(22, 23, 24)     # pins for cue, touch sensor, solenoid
+# Create spouts
+spouts = []
+if p.spout_count == 1:
+    spouts.append(spout.Spout(22, 23, 24))     # pins for cue, touch sensor, solenoid
+else:
+    print("Pins described for only one spout")
+    sys.exit(1)
 
-# Trap signals
-signal.signal(signal.SIGINT, handle_signal)
+# Trap INT signal
+signal.signal(signal.SIGINT, helpers.handle_signal)
+
+# initialise randomness
+random.seed()
 
 # Record start time
-start_time = time()
-end_time = start_time + session_duration * 60
-now = start_time
+p.start_time = time()
+p.end_time = p.start_time + p.session_duration * 60
+now = p.start_time
+
+
+## State 1 - inter-trial interval
+def iti(p):
+    print("Starting new trial")
+    ITI_duration = random.uniform(p.ITI_min, p.ITI_max)
+    outcome = True
+    return outcome
+
+
+## State 2 - trial period
+def trial(p, current_spout):
+    print("Current spout: %s. trial." % current_spout)
+
+
+## State 3 - reward period
+def reward(p, current_spout):
+    print("Current spout: %s. reward." % current_spout)
 
 
 ## Main control
-while now < end_time:
+while now < p.end_time:
+    # select a spout for this trial
+    current_spout = spout.select_spout(p.spout_count)
+
+    # start intertrial interval
+    outcome = iti(p)
+
     sleep(1)
     now = time()
 
 
+##
 """ sequence:
 1. ITI - static state
 2. while count down n seconds with no change to paw_l and paw_r
