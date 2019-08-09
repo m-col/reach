1b9960018554601c6187b72151ecbf9576d09410
mcol <mcol@posteo.net>
2019-08-09 17:09:44 +0100

    print seconds elapsed in trial start header

--- reach/session.py
+++ reach/session.py
@@ -140,9 +140,9 @@ class Session(object):
         while now < self.end_time:
             self.trial_count += 1
             self.success = False
-            print("_________________________________")
-            print("# ----- Starting trial #%i ----- #"
-                    % self.trial_count)
+            print("________________________________________")
+            print("# ---- Starting trial #%i ---- %s ---- #"
+                    % (self.trial_count, now-self.start_time))
 
             self.current_spout = random.randint(0, self.spout_count - 1)
             self.iti()
