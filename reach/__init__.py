ab75926a3dc9de0d41bd36e19807a4fe656b5d8e
mcol <mcol@posteo.net>
2019-09-08 23:30:56 +0100

    add setup.py

--- reach/__init__.py
+++ reach/__init__.py
@@ -1,4 +1,11 @@
+__version__ = '2019.09.08.0'
+
+from .cohort import Cohort
 from .mouse import Mouse
 from .session import Session
 
-__all__ = ['Mouse', 'Session', ]
+__all__ = [
+    'Cohort',
+    'Mouse',
+    'Session',
+]
---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+from os.path import join
+import re
+
+
+def get_version():
+    with open(join('reach', '__init__.py'), 'r') as f:
+        content = f.read()
+    p = re.compile(r'^__version__ = [\'"]([^\'\"]*)[\'"]', re.M)
+    return p.search(content).group(1)
+
+
+with open("readme.rst", "r") as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="reach",
+    version=get_version(),
+    author="Matt Colligan",
+    author_email="matt.colligan@ed.ac.uk",
+    description="Visually-guided reaching task for mice",
+    keywords=[],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DuguidLab/reach",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "matplotlib",
+        "seaborn",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX",
+    ],
+)
