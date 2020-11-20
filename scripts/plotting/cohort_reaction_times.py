b7ff8e697695d3fd3f64a12af3e5c5fe3c14b517
mcol <mcol@posteo.net>
2020-11-20 12:07:32 +0000

    fix cohort_reaction_times script

--- scripts/plotting/cohort_reaction_times.py
+++ scripts/plotting/cohort_reaction_times.py
@@ -6,20 +6,25 @@ Example script for plotting reaction times across all sessions for a cohort of m
 
 import os
 
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import sys
 import pandas as pd
 
 from reach import Cohort
+from reach.session import Outcomes
 
 
 def main(cohort):
     mouse = 0
-    rts = pd.DataFrame(cohort.mice[mouse].reaction_times).transpose()
+    trials = pd.DataFrame(cohort.get_trials())
+    trials["reaction_time"] = trials.end - trials.start
+    trials.loc[trials["outcome"] == Outcomes.MISSED, "reaction_time"] = np.nan
+    trials.loc[trials["outcome"] == Outcomes.CANCELLED, "reaction_time"] = np.nan
 
     sns.stripplot(
-        data=cohort.trials,
+        data=trials,
         x='day',
         y='reaction_time',
         hue='mouse_id',
