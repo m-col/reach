900025a323aa19b1e59deae3ae3f6bac90873e8d
mcol <mcol@posteo.net>
2021-06-25 15:15:50 +0100

    fix typo in utility

--- scripts/utilities/dispense_reward_volume.py
+++ scripts/utilities/dispense_reward_volume.py
@@ -6,6 +6,6 @@ Dispense a fixed volume (by time) of water from the spouts.
 from reach.backends.raspberrypi import Utilities
 
 rpi = Utilities(reward_duration=0.070)
-rpi.enabled_leds()
+rpi.enable_leds()
 rpi.dispense_reward_volume()
 rpi.cleanup()
